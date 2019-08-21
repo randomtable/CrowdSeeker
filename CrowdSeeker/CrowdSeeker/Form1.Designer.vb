232768bf14f75077ca9a527cc126247861e998f7
randomtable <alefiori3@gmail.com>
2019-08-21 15:46:52 +0200

    CrowdSeeker 1.1
    Added analysis for domain lists

---
+++ CrowdSeeker/CrowdSeeker/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Inserisci il Dominio:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(260, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(260, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Inserisci la profondità:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(260, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "START"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 119)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(260, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Processa Lista Domini"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "File di testo (*.txt)|*.txt"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CrowdSeeker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ CrowdSeeker/CrowdSeeker/Form1.vb
@@ -0,0 +1,219 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text
+
+Public Class Form1
+
+    Dim nomefile As String = "Result.txt"
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'processa("google.com", 10)
+    End Sub
+
+    Public Sub processa(ByVal dominio As String, ByVal deepness As Integer)
+        Dim risultato As String = analizza(dominio)
+        Dim dati() As String = risultato.Split(",")
+        For i = 0 To dati.Length - 1
+            If dati(i).Contains("DOMAIN") Then
+                File.AppendAllText("Result.txt", dominio & ";" & "DOMAIN" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("IP") Then
+                File.AppendAllText("Result.txt", dominio & ";" & "IP" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("EMAIL") Then
+                File.AppendAllText("Result.txt", dominio & ";" & "EMAIL" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("MD5") Then
+                File.AppendAllText("Result.txt", dominio & ";" & "MD5" & ";" & dati(i + 1) & ";" & "https://www.virustotal.com/gui/search/" & dati(i + 1) & vbCrLf)
+            End If
+        Next
+        cicla(deepness)
+    End Sub
+
+    Public Sub cicla(ByVal deepness As Integer)
+        If deepness = 0 Then
+            Exit Sub
+        End If
+        For i = 0 To deepness
+            Dim myFileStream As FileStream
+            Dim myStreamReader As StreamReader
+
+            Dim StreamEncoding As Encoding
+            StreamEncoding = Encoding.Default
+
+            Try
+                myFileStream = New FileStream(nomefile, FileMode.Open, FileAccess.Read)
+                myStreamReader = New StreamReader(myFileStream, StreamEncoding)
+                nomefile = "Result" & i & ".txt"
+                While myStreamReader.Peek <> -1
+                    Dim stringa As String = myStreamReader.ReadLine
+                    Dim datistringa() As String = stringa.Split(";")
+                    Dim richiesta As String = datistringa(2)
+                    Dim tipo As String = datistringa(1)
+                    If tipo = "DOMAIN" Then
+                        processadominio(richiesta)
+                    End If
+                    If tipo = "IP" Then
+                        processaip(richiesta)
+                    End If
+                    If tipo = "EMAIL" Then
+                        processaemail(richiesta)
+                    End If
+                    If tipo = "MD5" Then
+                        processamd5(richiesta)
+                    End If
+                End While
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                myStreamReader.Close()
+                myFileStream.Close()
+            End Try
+        Next
+    End Sub
+
+    Public Function analizza(ByVal dominio As String) As String
+        Dim inStream As StreamReader
+        Dim wr As WebRequest
+        Dim webresponse As WebResponse
+        wr = WebRequest.Create("https://www.threatcrowd.org/searchApi/v1/api.php?type=domain&query=" & dominio)
+        webresponse = wr.GetResponse()
+        inStream = New StreamReader(webresponse.GetResponseStream())
+        Return inStream.ReadToEnd()
+    End Function
+
+    Public Sub processadominio(ByVal dominio As String)
+        Dim risultato As String = analizza(dominio)
+        Dim dati() As String = risultato.Split(",")
+        For i = 0 To dati.Length - 1
+            If dati(i).Contains("DOMAIN") Then
+                File.AppendAllText(nomefile, dominio & ";" & "DOMAIN" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("IP") Then
+                File.AppendAllText(nomefile, dominio & ";" & "IP" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("EMAIL") Then
+                File.AppendAllText(nomefile, dominio & ";" & "EMAIL" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("MD5") Then
+                File.AppendAllText(nomefile, dominio & ";" & "MD5" & ";" & dati(i + 1) & ";" & "https://www.virustotal.com/gui/search/" & dati(i + 1) & vbCrLf)
+            End If
+        Next
+    End Sub
+
+    Public Sub processaip(ByVal dominio As String)
+        Dim risultato As String = analizzaip(dominio)
+        Dim dati() As String = risultato.Split(",")
+        For i = 0 To dati.Length - 1
+            If dati(i).Contains("DOMAIN") Then
+                File.AppendAllText(nomefile, dominio & ";" & "DOMAIN" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("IP") Then
+                File.AppendAllText(nomefile, dominio & ";" & "IP" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("EMAIL") Then
+                File.AppendAllText(nomefile, dominio & ";" & "EMAIL" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("MD5") Then
+                File.AppendAllText(nomefile, dominio & ";" & "MD5" & ";" & dati(i + 1) & ";" & "https://www.virustotal.com/gui/search/" & dati(i + 1) & vbCrLf)
+            End If
+        Next
+    End Sub
+
+    Public Function analizzaip(ByVal dominio As String) As String
+        Dim inStream As StreamReader
+        Dim wr As WebRequest
+        Dim webresponse As WebResponse
+        wr = WebRequest.Create("https://www.threatcrowd.org/searchApi/v1/api.php?type=ip&query=" & dominio)
+        webresponse = wr.GetResponse()
+        inStream = New StreamReader(webresponse.GetResponseStream())
+        Return inStream.ReadToEnd()
+    End Function
+
+    Public Sub processaemail(ByVal dominio As String)
+        Dim risultato As String = analizzaemail(dominio)
+        Dim dati() As String = risultato.Split(",")
+        For i = 0 To dati.Length - 1
+            If dati(i).Contains("DOMAIN") Then
+                File.AppendAllText(nomefile, dominio & ";" & "DOMAIN" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("IP") Then
+                File.AppendAllText(nomefile, dominio & ";" & "IP" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("EMAIL") Then
+                File.AppendAllText(nomefile, dominio & ";" & "EMAIL" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("MD5") Then
+                File.AppendAllText(nomefile, dominio & ";" & "MD5" & ";" & dati(i + 1) & ";" & "https://www.virustotal.com/gui/search/" & dati(i + 1) & vbCrLf)
+            End If
+        Next
+    End Sub
+
+    Public Function analizzaemail(ByVal dominio As String) As String
+        Dim inStream As StreamReader
+        Dim wr As WebRequest
+        Dim webresponse As WebResponse
+        wr = WebRequest.Create("https://www.threatcrowd.org/searchApi/v1/api.php?type=email&query=" & dominio)
+        webresponse = wr.GetResponse()
+        inStream = New StreamReader(webresponse.GetResponseStream())
+        Return inStream.ReadToEnd()
+    End Function
+
+    Public Sub processamd5(ByVal dominio As String)
+        Dim risultato As String = analizzamd5(dominio)
+        Dim dati() As String = risultato.Split(",")
+        For i = 0 To dati.Length - 1
+            If dati(i).Contains("DOMAIN") Then
+                File.AppendAllText(nomefile, dominio & ";" & "DOMAIN" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("IP") Then
+                File.AppendAllText(nomefile, dominio & ";" & "IP" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("EMAIL") Then
+                File.AppendAllText(nomefile, dominio & ";" & "EMAIL" & ";" & dati(i + 1) & ";" & vbCrLf)
+            End If
+            If dati(i).Contains("MD5") Then
+                File.AppendAllText(nomefile, dominio & ";" & "MD5" & ";" & dati(i + 1) & ";" & "https://www.virustotal.com/gui/search/" & dati(i + 1) & vbCrLf)
+            End If
+        Next
+    End Sub
+
+    Public Function analizzamd5(ByVal dominio As String) As String
+        Dim inStream As StreamReader
+        Dim wr As WebRequest
+        Dim webresponse As WebResponse
+        wr = WebRequest.Create("https://www.threatcrowd.org/searchApi/v1/api.php?type=md5&query=" & dominio)
+        webresponse = wr.GetResponse()
+        inStream = New StreamReader(webresponse.GetResponseStream())
+        Return inStream.ReadToEnd()
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        processa(TextBox1.Text, TextBox2.Text)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        OpenFileDialog1.ShowDialog()
+        Dim myFileStream As FileStream
+        Dim myStreamReader As StreamReader
+
+        Dim StreamEncoding As Encoding
+        StreamEncoding = Encoding.Default
+
+        Try
+            myFileStream = New FileStream(OpenFileDialog1.FileName, FileMode.Open, FileAccess.Read)
+            myStreamReader = New StreamReader(myFileStream, StreamEncoding)
+            While myStreamReader.Peek <> -1
+                processa(myStreamReader.ReadLine, TextBox2.Text)
+            End While
+
+        Catch ex As Exception
+
+        Finally
+            myStreamReader.Close()
+            myFileStream.Close()
+        End Try
+
+    End Sub
+End Class
